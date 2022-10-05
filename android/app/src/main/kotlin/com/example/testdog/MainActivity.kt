584280177c5b45883e674bf1270dd062e8520540
suryapradipta.github.io <63577853+suryapradipta@users.noreply.github.com>
2022-10-05 11:48:09 +0800

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/testdog/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.testdog
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'home.dart';
+
+List<CameraDescription>? cameras;
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  cameras = await availableCameras();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'Dogs Breed Recognizer',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Home(),
+    );
+  }
+}
